5378a05d012b8eb9b5c510f61391c7cf6acc1af8
Daniel Ramirez <ramirezdani016@hotmail.com>
2023-06-25 20:04:57 -0300

    cambios en cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,36 +1,57 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { CartContext } from "../context/cartContext";
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Button } from "react-bootstrap";
 
 export default function Cart() {
     const { getItems, removeItem } = useContext(CartContext)
+    const [products, setProducts] = useState([])
 
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-5">
-                    <ListGroup as="ul">
-                        {
-                            getItems().map(prod => {
-                                return (
-                                    <ListGroup.Item as="li" key={prod.id}
-                                        className="d-flex justify-content-between align-items-start">
-                                        <div className="ms-2 me-auto">
-                                            <div className="fw-bold">{prod.title}</div>
-                                            Cantidad: {prod.count}
-                                        </div>
-                                        <Button variant="primary" size="sm"
-                                            onClick={() => removeItem(prod.id)}>
-                                            x
-                                        </Button>
-                                    </ListGroup.Item>
-                                )
-                            })
-                        }
-                    </ListGroup>
+    useEffect(() => {
+        setProducts(getItems())
+    }, [getItems])
+
+    if (products.length > 0) {
+        return (
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-5">
+                        <ListGroup as="ul">
+                            {
+                                products.map(prod => {
+                                    return (
+                                        <ListGroup.Item as="li" key={prod.id}
+                                            className="d-flex justify-content-between align-items-start">
+                                            <div className="ms-2 me-auto">
+                                                <div className="fw-bold">{prod.title}</div>
+                                                Cantidad: {prod.count}
+                                            </div>
+                                            <Button variant="primary" size="sm"
+                                                onClick={() => removeItem(prod.id)}>
+                                                x
+                                            </Button>
+                                        </ListGroup.Item>
+                                    )
+                                })
+                            }
+                        </ListGroup>
+                    </div>
+
+                    <div className="col-5">
+                        <h3 className="text-center">Total de la compra: $</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="cointainer mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <h1>El carrito está vacío</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,16 +1,22 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { CartContext } from "../context/cartContext";
 import { Link } from "react-router-dom";
 
 export default function CartWidget() {
     const { countItems } = useContext(CartContext)
+    const [count, setCount] = useState()
+
+    useEffect(() => {
+        setCount(countItems())
+    }, [countItems])
 
     return (
-        <>
-            <Link to={'cart'} className="nav-link">
-                <i className="bi bi-cart" style={{ fontSize: '35px', marginRight: '5px' }}></i>
-                <span style={{ fontSize: '35px' }}>{countItems()}</span>
-            </Link>
-        </>
+        <Link to={'cart'} className="nav-link">
+            <i className="bi bi-cart" style={{ fontSize: '35px', marginRight: '5px' }}></i>
+            {
+                count > 0 &&
+                <span style={{ fontSize: '35px' }}>{count}</span>
+            }
+        </Link>
     );
 }
